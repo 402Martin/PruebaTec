89992f0a06404d18bba079785a5962180a8afe5b
guillejude <402martin@gmail.com>
2022-06-11 18:33:16 -0300

    small refactor

--- App/src/services/transaction-services.ts
+++ App/src/services/transaction-services.ts
@@ -1,37 +1,10 @@
 import Database from '../db/database';
 import { Transaction } from '../types';
+import { getAccount, isOwner } from './account-service';
 
 const { Op } = require('sequelize');
 
 const { models } = Database.mysql;
-
-const isOwner = async (idDocument: number, idAccount: number) => {
-  const user = await models.User.findOne({ where: { idDocument } });
-  const where: { userId: number; id?: number } = idAccount
-    ? { userId: user.id, id: idAccount }
-    : { userId: user.id };
-
-  const isOwnerOfAccounts = await models.Account.findAll({
-    where,
-  });
-  return isOwnerOfAccounts;
-};
-
-const getAccount = async (idDocument: number, idAccount: number) => {
-  const user = await models.User.findOne({ where: { idDocument } });
-  const where: { userId: number; id?: number } = {
-    userId: user.id,
-    id: idAccount,
-  };
-
-  const account = await models.Account.findOne({
-    where,
-  });
-
-  if (!account) throw new Error('Account not found');
-
-  return account;
-};
 
 const createTransaction = async (
   transactionIn: Transaction,
