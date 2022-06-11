def8b328e59a2f6f6dde870ae6e293e35cc4a17b
guillejude <402martin@gmail.com>
2022-06-11 17:41:02 -0300

    get validations

--- App/src/services/transaction-services.ts
+++ App/src/services/transaction-services.ts
@@ -5,6 +5,21 @@ const { Op } = require('sequelize');
 
 const { models } = Database.mysql;
 
+const isOwner = async (idDocument: number, idAccount: number) => {
+  const user = await models.User.findOne({ where: { idDocument } });
+  const where: { userId: number; id?: number } = idAccount
+    ? { userId: user.id, id: idAccount }
+    : { userId: user.id };
+
+  const isOwnerOfAccounts = await models.Account.findAll({
+    where,
+  });
+  if (!isOwnerOfAccounts) {
+    throw new Error(`Account dosn't belong to this user`);
+  }
+  return isOwnerOfAccounts;
+};
+
 const createTransaction = async (
   transactionIn: Transaction,
 ): Promise<Transaction> => {
@@ -12,15 +27,20 @@ const createTransaction = async (
   const transaction = await models.Transaction.create(transactionIn);
   return transaction;
 };
-const getTransactions = async (query: any): Promise<Transaction> => {
-  const where: { date?: any; accountFrom?: number } = {};
+
+const getTransactions = async (
+  query: any,
+  idDocument: number,
+): Promise<Transaction> => {
+  const accounts = await isOwner(idDocument, query.sourceAccountId);
+
+  const where: { date?: any; accountFrom?: any } = {
+    accountFrom: { [Op.in]: accounts.map((account) => account.id) },
+  };
 
   if (query.from && query.to)
     where.date = { [Op.between]: [query.from, query.to] };
   else if (query.from) where.date = { [Op.gte]: query.from };
-  else if (query.to) where.date = { [Op.lte]: query.to };
-  if (query.sourceAccountId) where.accountFrom = query.sourceAccountId;
-
   const transaction = await models.Transaction.findAll({
     where,
   });
