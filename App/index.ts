e29fc5c9b0bbf072230b2962bc12d16377d414cd
guillejude <402martin@gmail.com>
2022-06-12 00:24:41 -0300

    devleopment server

--- App/index.ts
+++ App/index.ts
@@ -4,14 +4,17 @@ import serverInit from './src/server';
 import { logger } from './src/utils';
 import insertMockedData from './src/utils/mock-data/imports';
 
+const config = require('./src/env.json')[process.env.NODE_ENV || 'development'];
+
 (async () => {
   try {
     const success = await Database.init();
     if (!success) throw new Error('Databases init failed');
 
-    const succesInserts = await insertMockedData();
-
-    if (!succesInserts) throw new Error('Databases init failed');
+    if (config.development) {
+      const succesInserts = await insertMockedData();
+      if (!succesInserts) throw new Error('Databases inserts failed');
+    }
 
     await serverInit();
 
--- App/src/helpers/cron-helper.ts
+++ App/src/helpers/cron-helper.ts
@@ -2,10 +2,12 @@ import cron from 'node-cron';
 import setCurrencies from '../services/currency-service';
 import { logger } from '../utils';
 
+const config = require('../env.json')[process.env.NODE_ENV || 'development'];
+
 const currencyCron = async () => {
   cron.schedule('* * * * *', async () => {
     logger.info('Starting running  Currency cron');
-    await setCurrencies();
+    if (!config.development) await setCurrencies();
     logger.info('Finished running currency cron');
   });
 };
