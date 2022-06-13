2153fb18da7dd66c8375dcb5e3be1d5b4d8a5ab4
guillejude <402martin@gmail.com>
2022-06-13 16:32:33 -0300

    cron fix

--- App/src/services/currency-service.ts
+++ App/src/services/currency-service.ts
@@ -1,19 +1,24 @@
 import Database from '../db/database';
 import getQuotes from '../helpers/fixer-helper';
+import { logger } from '../utils';
 
 const { models } = Database.mysql;
 
 const setCurrencies = async () => {
-  const rates = await getQuotes();
+  try {
+    const rates = await getQuotes();
 
-  await models.Currency.update(
-    { eurRate: rates.UYU },
-    { where: { idCurrency: 'UYU' } },
-  );
-  await models.Currency.update(
-    { eurRate: rates.USD },
-    { where: { idCurrency: 'USD' } },
-  );
+    await models.Currency.update(
+      { eurRate: rates.UYU },
+      { where: { idCurrency: 'UYU' } },
+    );
+    await models.Currency.update(
+      { eurRate: rates.USD },
+      { where: { idCurrency: 'USD' } },
+    );
+  } catch (err) {
+    logger.error(err);
+  }
 };
 
 export default setCurrencies;
